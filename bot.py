6834e7e269bc455b5aa07990c076f585559b5ce7
Niharika Arora <aroraniharika15@gmail.com>
2020-08-11 23:17:24 +0530

    Tag 'a' links scraped from the URL content

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import xlrd
 import os
+import re
+import requests
+import pandas
+from bs4 import BeautifulSoup
 
 
 loc = os.path.abspath('websites.xlsx')
@@ -12,5 +16,20 @@ url = ''
 
 for i in range(st.nrows):
     if site == str(st.cell_value(i, 0)):
-        url = st.cell_value(i, 1)
+        url = str(st.cell_value(i, 1))
+if url == '':
+    url = "URL Not Found!"
 print(url)
+
+headers = {"Accept-Language": "en-US, en;q=0.5"}
+r = requests.get(url, headers = headers)
+txt = BeautifulSoup(r.text, 'html.parser')
+
+links = []
+
+for link in txt.findAll('a'):
+    links.append(link.get('href'))
+print(links)
+
+mail_list = re.findall('[a-z0-9_\-\.]+@[a-z\.]+\.[a-z]{2,3}')
+phone = re.findall('(\+(\d\-)?\d{2,3}[\s\-]?)?(\d{10}|(\(\d{3}\)([\s\-]?)\d{3}\5\d{4})|(\d{3}([\s\-]?)\d{3}\7\d{4}))')
