5dbe3aec40d884cf811fdf3fd80467813a0fb939
Niharika Arora <aroraniharika15@gmail.com>
2020-08-12 11:49:36 +0530

    Fetching filtered text

--- bot.py
+++ bot.py
@@ -4,7 +4,18 @@ import re
 import requests
 import pandas
 from bs4 import BeautifulSoup
+from bs4.element import Comment
+import urllib.request
 
+
+headers = {"Accept-Language": "en-US, en;q=0.5"}
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
 
 def find_link(url):
     r = requests.get(url, headers = headers)
@@ -22,18 +33,21 @@ def contact_check(links):
     return url1
 
 def save_file(r, url):
-    soup = BeautifulSoup(r.text, 'html.parser')
-    txt = soup.findALL('div', class_ = "col")
-    mail_list = re.findall('[a-z0-9_\-\.]+@[a-z\.]+\.[a-z]{2,3}', txt)
-    phone = re.findall('(\+(\d\-\ )?\d{2,3}[\s\-]?)?(\d{10}|(\(\d{3}\)([\s\-]?)\d{3}\5\d{4})|(\d{3}([\s\-]?)\d{3}\7\d{4}))', txt)
+    body = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(body, 'html.parser')
+    txt = soup.findALL(text=True)
+    visible_texts = filter(tag_visible, txt) 
+    mail_list = re.findall('[a-z0-9_\-\.]+@[a-z\.]+\.[a-z]{2,3}', str(visible_texts))
+    phone = re.findall('(\+(\d\-\ )?\d{2,3}[\s\-]?)?(\d{10}|(\(\d{3}\)([\s\-]?)\d{3}\5\d{4})|(\d{3}([\s\-]?)\d{3}\7\d{4}))', str(visible_texts))
     dicn = {'link': url, 'email': mail_list, 'phone number': phone}
+    for i in dicn.keys():
+        print(dicn[i])
     df = pandas.DataFrame.from_dict(dicn, orient= 'index')
     df.transpose()
     df.to_csv('contact_info.csv')
     print("File Saved!!")
 
 if __name__ == "__main__":
-    headers = {"Accept-Language": "en-US, en;q=0.5"}
     loc = os.path.abspath('websites.xlsx')
     wb = xlrd.open_workbook(loc)
     st = wb.sheet_by_index(0)
